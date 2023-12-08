b55a14e9d1bf6f8be68b6c648256dacff5ee93e0
Ivan Shaymardanov <uvixmi@mail.ru>
2023-12-08 13:02:52 +0300

    feat(*): update reg

--- src/components/register-page/index.tsx
+++ src/components/register-page/index.tsx
@@ -307,10 +307,10 @@ export const RegisterPage = ({
           inn,
         })
       )
-    // await api.users.saveTaxInfoUsersTaxInfoPut(
-    //   { inn, tax_rate: parseInt(rate.slice(0, -1), 10), start_year: startYear },
-    //   { headers }
-    // )
+    await api.users.saveTaxInfoUsersTaxInfoPut(
+      { inn, tax_rate: parseInt(rate.slice(0, -1), 10), start_year: startYear },
+      { headers }
+    )
     if (sno == TaxSystemType.UsnD) setOpen(true)
     else navigate("/non-target")
   }
